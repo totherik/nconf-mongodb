cfe669b1734016851e1a304a0a6cea9ba9d47a62
ertoth <ertoth@paypal.com>
2012-05-08 21:26:00 -0700

    extend nconf to support mongodb

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var nconf = require('nconf'),
 	lib = null;
 
-nconf.__defineGetter__('MongoDB', function () {
-	return lib || (lib = require('./lib/nconf-mongodb').MongoDB);
+nconf.__defineGetter__('Mongodb', function () {
+	return lib || (lib = require('./lib/nconf-mongodb').Mongodb);
 });
--- test/test.js
+++ test/test.js
@@ -6,10 +6,15 @@ require('../index');
 
 describe('nconf-mongodb', function() {
 	
-	var mongodb = nconf.MongoDB;
+	var mongodb = nconf.Mongodb;
 	
 	it('should support the mongodb type', function(next) {
 		should.exist(mongodb);
+		next();
+	});
+	
+	it('should be initialized through nconf', function(next) {
+		nconf.use('mongodb', { host: 'localhost', port: 27017 });
 		next();
 	});
 	
