01f33063fea0657788a2a70d97693fae26558bd1
David Burhans <david.burhans@corvisa.com>
2014-06-23 09:58:36 -0600

    Added a couple of tests

--- test/test.js
+++ test/test.js
@@ -6,9 +6,11 @@ require('../index');
 var testKey = 'test'
 	, expected = 'expected-value'
 
+var mongoOpts = { db: 'nconf-mongodb-test', host: 'localhost', port: 27017 };
+
 function getMongo() {
 	var conf = new nconf.Provider();
-	conf.use('mongodb', { db: 'nconf-mongodb-test', host: 'localhost', port: 27017 });
+	conf.use('mongodb', mongoOpts);
 	return conf;
 }
 
@@ -44,10 +46,39 @@ describe('nconf-mongodb', function() {
 				var conf2 = getMongo();
 				conf2.load(function(loadErr, doc) {
 					should.not.exist(loadErr);
-					should.exist(doc);
 					conf2.get(testKey).should.equal(expected);
 					next();
 				})
+			});
+		});
+	});
+
+	it('should clear the key when clear is called', function(next) {
+		var conf = getMongo();
+		conf.load(function(err, docs) {
+			should.not.exist(err);
+			conf.set(testKey, expected);
+			conf.save(function(saveErr) {
+				should.not.exist(saveErr);
+				var conf2 = getMongo();
+
+				conf2.load(function(loadErr, doc) {
+					should.not.exist(loadErr);
+					should.exist(doc);
+					conf2.get(testKey).should.equal(expected);
+					conf2.clear(testKey);
+					should.not.exist(conf2.get(testKey));
+					next();
+				});
+			});
+		});
+	});
+
+	it('should remove the keys from mongo on reset', function(next) {
+		var mongo = new nconf.Mongodb(mongoOpts);
+		mongo.load(function(err, doc) {
+			mongo.reset(function() {
+				next();
 			});
 		});
 	});
